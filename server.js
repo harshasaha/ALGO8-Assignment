d88a6e7cebd6a7088aa9ccd60233590a507c7bd6
harshaatteachonapp <harsha@teachonapp.com>
2020-12-13 15:36:28 +0530

    port change

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ MongoClient.connect(
 			// ...
 		});
 
-		var server = app.listen(3000, function () {
+		var server = app.listen(process.env.PORT, function () {
 			var host = server.address().address;
 			var port = server.address().port;
 			console.log("Example app listening at http://%s:%s", host, port);
